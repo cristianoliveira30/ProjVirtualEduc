ee86b8660001bbd5b0d17fed159878c36681f3e9
Davi-04 <davidalves.cm.inf.int@gmail.com>
2023-09-05 10:00:00 -0300

    alteração

--- cadastro.php
+++ cadastro.php
@@ -3,6 +3,7 @@
 <head>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <link rel="stylesheet" href="./css/cadastro.css">
+  <link rel="stylesheet" href="./css/cadastro.scss"><!-- Arquivo para ajustar o select-->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cadastro</title>
