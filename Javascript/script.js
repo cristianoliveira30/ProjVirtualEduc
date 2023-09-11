850a8a9a303b00be1843c4e59f5c124f5d8f8f0b
Davi-04 <davidalves.cm.inf.int@gmail.com>
2023-09-10 21:09:54 -0300

    teste07

--- Javascript/script.js
+++ Javascript/script.js
@@ -8,18 +8,6 @@
  * a janela modal. Esse evento acontecera, quando o botão for clicado
 */
 
-/*const btn = document.querySelector("#btn-submit");
-const container = document.querySelector(".container1");
-btn.addEventListener('click', clicar);
-
-btn.addEventListener("click", clicar);
-function clicar() {
-    if (container.style.display === "block") {
-        container.style.display = "none";
-    } else {
-        container.style.display = "block";
-    }
-}*/
 
 // scripts do Luke
 
@@ -42,10 +30,14 @@ function clicar() {
 
 // Interrompe o carregamento após 5 segundos (ajuste conforme necessário)
 
-let btn = document.querySelector("#btn");
-let container0 = document.querySelector(".FromCadastro1");
-let container1 = document.querySelector(".FromCadastro1");
+
 
 btn.addEventListener("click", function() {
-    container0.style.display = 'none';
+    const btn = document.querySelector('#btn');
+    const form1 = document.querySelector('.FromCadastro');
+    const form2 = document.querySelector('.FromCadastro1');
+    
+    form1.style.display = "none";
+    form2.style.display = "block";
 });
+
