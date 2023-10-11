dd79c0a8d3c7ab160262b9f58c5a167566e98051
Davi-04 <davidalves.cm.inf.int@gmail.com>
2023-10-11 16:55:32 -0300

    criando script para o slider

--- js/script.js
+++ js/script.js
@@ -53,3 +53,22 @@ masc.addEventListener("keyup", function () {
         telefone.value += "(";
     }
 });
+
+// Sliders 
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function () {
+    nextImagem();
+}, 5000)
+
+function nextImagem() {
+    count++;
+    if (count > 4) {
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+
+}
