1e5cf90356f63fdd40d2f1bdd4c540c3b5cd7652
Cristian0800 <cristianroan30@gmail.com>
2023-10-12 16:18:26 -0300

    ajeitando responsividade do index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 	<link rel="stylesheet" href="./css/style.css">
-
+  <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Home</title>
@@ -12,7 +12,7 @@
 
 <body class="bg-animated">
 
-	<script>
+	<script> //
 		// Event listener para detectar quando o conteúdo da página foi carregado
 		window.addEventListener("load", function() {
 			var boxad = document.getElementById("boxad");
@@ -26,7 +26,7 @@
 		}, 5000); // Interrompe o carregamento após 5 segundos (ajuste conforme necessário)
 	</script>
 
-	<div class="boxad" id="boxad">
+	<div class="boxad" id="boxad"> 
 		<div class="content">
 
 
@@ -47,52 +47,13 @@
 		</div>
 	</div>
 	<header>
-		<!--navbar-->
-		<nav class="navbar navbar-expand-lg bg-dark">
-			<div class="container-fluid">
-				<a class=" link-light navbar-brand" href="#">VirtualEduc</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav">
-						<li class="nav-item">
-							<a class=" link-light nav-link active" aria-current="page" href="#">Como Usar</a>
-						</li>
-						<li class="nav-item">
-							<a class=" link-light nav-link" href="#">Quem somos</a>
-						</li>
-						<li class="nav-item dropdown">
-							<a class=" link-light nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-								Dropdown
-							</a>
-							<ul class="dropdown-menu bg-dark">
-								<li><a class=" link-light dropdown-item" href="#">Action</a></li>
-								<li><a class=" link-light dropdown-item" href="#">Another action</a></li>
-								<li>
-									<hr class="dropdown-divider">
-								</li>
-								<li><a class=" link-light dropdown-item" href="#">Something else here</a></li>
-							</ul>
-						</li>
-						<li class="nav-item">
-							<a class=" link-light nav-link disabled" aria-disabled="true">Acessar Conteúdo</a>
-						</li>
-					</ul>
-					<div name="buttons" class="position-absolute end-0 me-4">
-						<a class=" link-light btn btn-primary" href="./cadastro.php" role="button">Cadastre-se</a>
-						<a class=" link-light btn btn-info" href="./login.php" role="button">Entrar</a>
-					</div>
-				</div>
-			</div>
-		</nav>
 	</header>
 	<main>
 		<section class="">
 			<div class="">
 				<h1 class="text-center">Imagine um lugar...</h1>
 			</div>
-			<div class="DivText bg-dark rounded-4 position-absolute top-50 start-50 translate-middle border border-warning">
+			<div id="divtext" class="DivText bg-dark rounded-4 position-absolute top-50 start-50 translate-middle border border-warning p-1">
 				<p id="texto" class="fs-4 text-start ms-3 mt-2">
 					O texto...<span>|</span>
 				</p>
@@ -153,7 +114,13 @@
 	</main>
 	<footer>
 	</footer>
+  <script src="./js/script.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <script>
+    if (isMobile()) {
+      $("#divtext").removeClass("DivText").addClass("DivTextMobile");
+    }
+  </script>
 </body>
 
 </html>
